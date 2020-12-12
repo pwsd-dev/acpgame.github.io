dd3eb3f1b9db62b14ff1c5092130985b07178f79
iliagromov <ilia.workproffile@gmail.com>
2020-12-12 15:08:09 +0300

    add project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # acpgame.github.io
 acpgame
+Макет
+https://www.figma.com/file/0CdQGYuAI5YhyWDf4zZKzC/ACP-GAME-(Copy)?node-id=0%3A1
---
+++ smartgrid.js
@@ -0,0 +1,29 @@
+module.exports = {
+	columns: 6,
+	offset: "0px",
+	//mobileFirst: true,
+	container: {
+        maxWidth: "1246px",
+        fields: "90px"
+    },
+	breakPoints: {
+		md: {
+            width: "920px",
+            fields: "15px"
+        },
+        sm: {
+            width: "720px"
+        },
+        xs: {
+            width: "576px"
+        },
+        xxs: {
+            width: "420px",
+            /*
+            offset: "10px",
+            fields: "5px"
+            */
+        }
+	},
+    //detailedCalc: true
+};
