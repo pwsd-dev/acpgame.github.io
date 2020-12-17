7dcb9a35b64f4914e8ac7e9848c5aa448711b546
iliagromov <ilia.workproffile@gmail.com>
2020-12-17 12:55:28 +0300

    add banner adaptive

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,22 @@
+let bannerInfoLinkToggle = document.querySelector('.banner-info .page__link-toggle');
+let bannerInfoContent = document.querySelectorAll('.banner-info .banner-info-content .page__text');
+
+bannerInfoLinkToggle.addEventListener('click', (e)=>{
+  if(!e.target.classList.contains('page__link-toggle_hide')){
+    e.target.innerText = 'скрыть';
+
+  } else{
+    e.target.innerText = 'еще даты';
+  }
+  e.target.classList.toggle('page__link-toggle_hide');
+  
+  
+  bannerInfoContent.forEach((link)=>{
+    link.classList.toggle('hide_mobile');
+  });
+
+});
+
 
 var sliderBanner = tns({
     container: "#sliderBanner",
@@ -6,7 +25,7 @@ var sliderBanner = tns({
     // loop: true,
     navContainer: "#customizeThumbnails",
     navAsThumbnails: true,
-    autoplay: false,
+    autoplay: true,
     autoplayResetOnVisibility: false,
     controls: false,
     autoplayPosition: "bottom",
@@ -14,10 +33,11 @@ var sliderBanner = tns({
         "▶",
         "❚❚"
       ],
-    autoplayTimeout: 1500,
+    autoplayTimeout: 1800,
     swipeAngle: true,
     animateIn: "jello",
     animateOut: "rollOut",
     lazyload: true,
-    speed: 800
+    speed: 800,
+    
   });
